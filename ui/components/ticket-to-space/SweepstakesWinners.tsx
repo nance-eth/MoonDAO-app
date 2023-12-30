e15eb2f1c53398303afbd582823222aec6e1819a
colinmfoster4723 <colin.foster4723@gmail.com>
2023-12-29 18:08:40 -0600

    Adjusted getWinner function and styling

--- ui/components/ticket-to-space/SweepstakesWinners.tsx
+++ ui/components/ticket-to-space/SweepstakesWinners.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
 type Winner = {
   tokenId: string
   address: string
-  fullName: string
+  name: string
 }
 
 export function SweepstakesWinners({ ttsContract, supply }: any) {
@@ -16,28 +17,30 @@ export function SweepstakesWinners({ ttsContract, supply }: any) {
     const verifiedNftsData = await verifiedNftsRes.json()
     const { data: verifiedNfts } = verifiedNftsData
 
-    console.log(verifiedNfts)
+    for (let i = 0; i <= 10; i++) {
+      try {
+        const randomWordsId = await ttsContract.call('requestIds', [i])
+        if (randomWordsId) {
+          const { randomWords } = await ttsContract.call('getRequestStatus', [
+            randomWordsId,
+          ])
 
-    for (let i = 10; i > 0; i--) {
-      const randomWordsId = await ttsContract.call('requestIds', [0])
-      if (randomWordsId) {
-        const { randomWords } = await ttsContract.call('getRequestStatus', [
-          randomWordsId,
-        ])
+          const winningTokenId = await randomWords[0].mod(supply).toString()
 
-        const winningTokenId = await randomWords[0].mod(supply).toString()
+          const verifiedWinner = verifiedNfts.find(
+            (vNft: any) => vNft.tokenId === winningTokenId
+          )
 
-        const verifiedWinner = verifiedNfts.find(
-          (vNft: any) => vNft.tokenId === winningTokenId
-        )
+          const winner = {
+            tokenId: winningTokenId,
+            address: verifiedWinner?.address,
+            name: verifiedWinner?.name,
+          }
 
-        const winner = {
-          tokenId: winningTokenId,
-          address: verifiedWinner?.address,
-          fullName: verifiedWinner?.fullName,
+          winners.push(winner)
         }
-
-        winners.push(winner)
+      } catch (err) {
+        console.log(err)
       }
     }
     setWinners(winners)
@@ -54,18 +57,38 @@ export function SweepstakesWinners({ ttsContract, supply }: any) {
         {`One person will be randomly selected to win an opportunity aboard a future Blue Origin rocket to space!`}
       </p>
 
-      {winners.length > 0 && (
-        <div className="mt-5">
-          <h2 className="text-xl font-bold">Winners</h2>
-          <div className="mt-3">
+      <div className="mt-5">
+        <h2 className="text-xl font-bold">Winners</h2>
+        {winners.length > 0 ? (
+          <div className="flex flex-col max-h-[600px] overflow-y-scroll">
             {winners.map((winner: any, i: number) => (
-              <div key={i} className="mt-2">
-                <p className="text-base">{winner.fullName}</p>
-              </div>
+              <>
+                <div
+                  key={i}
+                  className="flex gap-4 px-5 lg:px-7 xl:px-10 py-6 page-border-and-color font-RobotoMono w-[336px] sm:w-[400px] lg:mt-10 lg:w-full lg:max-w-[1080px] text-slate-950 text-sm dark:text-white"
+                >
+                  <h1 className="font-[Goodtimes] text-2xl">{`#${10 - i}`}</h1>
+                  <div className="flex flex-col">
+                    <p>{`Name : ${winner.name}`}</p>
+                    <p>{`Token Id : ${winner.tokenId}`}</p>
+                    <Link
+                      href={'https://polygonscan.com/address/' + winner.address}
+                      target="_blank"
+                    >
+                      {`Address : ${winner.address.slice(
+                        0,
+                        6
+                      )}...${winner.address.slice(-4)}`}
+                    </Link>
+                  </div>
+                </div>
+              </>
             ))}
           </div>
-        </div>
-      )}
+        ) : (
+          <div className="mt-3 px-5 lg:px-7 xl:px-10 py-12 lg:py-14 page-border-and-color font-RobotoMono w-[336px] sm:w-[400px] lg:mt-10 lg:w-full lg:max-w-[1080px] text-slate-950 dark:text-white"></div>
+        )}
+      </div>
     </div>
   )
 }
