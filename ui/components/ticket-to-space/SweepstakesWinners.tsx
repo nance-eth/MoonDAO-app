8c1a295623e1f04238f0c4e021b0ba1a8beedb14
colinmfoster4723 <colin.foster4723@gmail.com>
2024-01-02 12:02:20 -0600

    Adjusted styling

--- ui/components/ticket-to-space/SweepstakesWinners.tsx
+++ ui/components/ticket-to-space/SweepstakesWinners.tsx
@@ -16,7 +16,6 @@ export function SweepstakesWinners({ ttsContract, supply }: any) {
     const verifiedNftsRes: any = await fetch('/api/db/nft')
     const verifiedNftsData = await verifiedNftsRes.json()
     const { data: verifiedNfts } = verifiedNftsData
-
     for (let i = 0; i <= 10; i++) {
       try {
         const randomWordsId = await ttsContract.call('requestIds', [i])
@@ -27,14 +26,18 @@ export function SweepstakesWinners({ ttsContract, supply }: any) {
 
           const winningTokenId = await randomWords[0].mod(supply).toString()
 
+          const ownerOfWinningTokenId = await ttsContract.call('ownerOf', [
+            winningTokenId,
+          ])
+
           const verifiedWinner = verifiedNfts.find(
             (vNft: any) => vNft.tokenId === winningTokenId
           )
 
           const winner = {
             tokenId: winningTokenId,
-            address: verifiedWinner?.address,
-            name: verifiedWinner?.name,
+            address: ownerOfWinningTokenId,
+            name: verifiedWinner?.name || 'Unverified',
           }
 
           winners.push(winner)
@@ -48,6 +51,7 @@ export function SweepstakesWinners({ ttsContract, supply }: any) {
 
   useEffect(() => {
     if (ttsContract && supply) getWinners()
+    console.log(winners)
   }, [ttsContract, supply])
 
   return (
@@ -60,29 +64,28 @@ export function SweepstakesWinners({ ttsContract, supply }: any) {
       <div className="mt-5">
         <h2 className="text-xl font-bold">Winners</h2>
         {winners.length > 0 ? (
-          <div className="flex flex-col max-h-[600px] overflow-y-scroll">
+          <div className="flex flex-col max-h-[600px] overflow-y-scroll items-center">
             {winners.map((winner: any, i: number) => (
-              <>
-                <div
-                  key={i}
-                  className="flex gap-4 px-5 lg:px-7 xl:px-10 py-6 page-border-and-color font-RobotoMono w-[336px] sm:w-[400px] lg:mt-10 lg:w-full lg:max-w-[1080px] text-slate-950 text-sm dark:text-white"
-                >
-                  <h1 className="font-[Goodtimes] text-2xl">{`#${10 - i}`}</h1>
-                  <div className="flex flex-col">
-                    <p>{`Name : ${winner.name}`}</p>
-                    <p>{`Token Id : ${winner.tokenId}`}</p>
-                    <Link
-                      href={'https://polygonscan.com/address/' + winner.address}
-                      target="_blank"
-                    >
-                      {`Address : ${winner.address.slice(
-                        0,
-                        6
-                      )}...${winner.address.slice(-4)}`}
-                    </Link>
-                  </div>
+              <div
+                key={`winner-${i}`}
+                className="flex gap-4 px-5 lg:px-7 xl:px-10 py-6 border-2 dark:border-[#ffffff20] font-RobotoMono w-[336px] sm:w-[400px] lg:mt-10 lg:w-3/4 lg:max-w-[1080px] text-slate-950 text-sm dark:text-white"
+              >
+                <h1 className="font-[Goodtimes] text-2xl">{`#${10 - i}`}</h1>
+                <div className="w-[2px] h-12 bg-[#00000020] dark:bg-[#ffffff20]" />
+                <div className="flex flex-col">
+                  <p>{`Name : ${winner.name}`}</p>
+                  <p>{`Token Id : ${winner.tokenId}`}</p>
+                  <Link
+                    href={'https://polygonscan.com/address/' + winner.address}
+                    target="_blank"
+                  >
+                    {`Address : ${winner?.address?.slice(
+                      0,
+                      6
+                    )}...${winner?.address?.slice(-4)}`}
+                  </Link>
                 </div>
-              </>
+              </div>
             ))}
           </div>
         ) : (
